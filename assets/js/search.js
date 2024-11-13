c57667e2a6feeb3cc18aba91d9f3df5e66c8378d
SomeBodyLite <danhik.maksamov@yandex.ru>
2024-11-13 12:13:51 +0700

    v0.9.3

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,33 +1,38 @@
-const searchWrapper = document.querySelector('.search-wrapper');
-const searchInput = document.querySelector('.main-search');
-const searchButton = document.querySelector('.search-button');
-const searchSuggestions = document.querySelector('.search-suggestions');
-const searchClearButton = document.querySelector('.search-button__clear');
+const searchWrapper = document.querySelector(".search-wrapper");
+const searchInput = document.querySelector(".main-search");
+const searchButton = document.querySelector(".search-button");
+const searchSuggestions = document.querySelector(".search-suggestions");
+const searchClearButton = document.querySelector(".search-button__clear");
 
-searchInput.addEventListener('input', function(e) {
+searchInput.addEventListener("input", function (e) {
   const length = e.target.value.length;
   if (length >= 2) {
-    searchSuggestions.classList.add('active');
+    searchSuggestions.classList.add("active");
   } else {
-    searchSuggestions.classList.remove('active');
+    searchSuggestions.classList.remove("active");
   }
 });
 
-searchButton.addEventListener('click', function(e) {
+searchButton.addEventListener("click", function (e) {
   e.preventDefault();
-  searchInput.classList.toggle('expanded');
-
+  searchInput.classList.toggle("expanded");
 });
 
-searchClearButton.addEventListener('click', function(e) {
+searchClearButton.addEventListener("click", function (e) {
   e.preventDefault();
-  searchInput.value = '';
-  searchSuggestions.classList.remove('active');
+  searchInput.classList.remove("expanded");
 });
 
-document.addEventListener('click', function(e) {
+// Вариант, который ОЧИЩАЕТ ИНПУТ ПРИ НАЖАТИИ НА КРЕСТИК
+
+// searchClearButton.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   searchInput.value = '';
+//   searchSuggestions.classList.remove('active');
+// });
+
+document.addEventListener("click", function (e) {
   if (!searchWrapper.contains(e.target)) {
-    searchInput.classList.remove('expanded');
+    searchInput.classList.remove("expanded");
   }
 });
-
